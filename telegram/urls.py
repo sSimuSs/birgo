00ddd10d9a1a77d62114bba8909556f14b538db8
Sirojiddin Mustafayev <sirojiddinmustafayev@gmail.com>
2025-09-18 21:10:56 +0500

    validate_user route

--- telegram/urls.py
+++ telegram/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from telegram.views import (
-    home, aut_error, init
+    home, aut_error, init, validate_user
 )
 
 urlpatterns = [
     path('tg/init/', init, name='tg_init'),
     path('tg/', home, name='tg_home'),
     path('tg/auth_error/', aut_error, name='tg_auth_error'),
+    path('tg/validate_user/', validate_user, name="validate_user"),
 ]
