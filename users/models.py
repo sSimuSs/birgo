295981a0fcacf9993a9084763393ec89a98532ab
Sirojiddin Mustafayev <sirojiddinmustafayev@gmail.com>
2025-09-18 12:39:39 +0500

    created users.BotUser model and installed pytelegrambotapi module

--- users/models.py
+++ users/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class BotUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    telegram_id = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    language_code = models.CharField(max_length=30, blank=True, null=True)
+    is_premium = models.BooleanField(default=False)
+    added_to_attachment_menu = models.BooleanField(default=False)
+    allows_write_to_pm = models.BooleanField(default=False)
+    photo_url = models.URLField(blank=True, null=True)
+    birthday = models.JSONField(default=dict, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    status = models.BooleanField(default=True)
+    has_left = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
